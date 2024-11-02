d3a35d6583daab05399b83bca8f373e5b0ad290d
Nabeel Rahman <mailnabeelrahman227@gmail.com>
2024-11-02 19:51:23 +0530

    added cloudinary configuration

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -2,3 +2,12 @@ const cloudinary = require("cloudinary").v2;
 const fs = require("fs")
 
 // Your Cloudinary configuration and usage here
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key:  process.env.CLOUDINARY_API_KEY, 
+        api_secret:  process.env.CLOUDINARY_CLOUD_SECRET 
+    });
+})
